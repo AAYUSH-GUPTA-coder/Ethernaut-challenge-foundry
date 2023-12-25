9e7c1eeaafa11087c3654a69e47faa8d4c0a5864
Aayush Gupta <guptaaayush432@gmail.com>
2023-12-25 13:02:24 +0530

    Added first challenge

---
+++ src/Fallback-01.sol
@@ -0,0 +1,44 @@
+// Objective
+// 1. you claim ownership of the contract
+// 2. you reduce its balance to 0
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Fallback {
+    mapping(address => uint256) public contributions;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+        contributions[msg.sender] = 1000 * (1 ether);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "caller is not the owner");
+        _;
+    }
+
+    function contribute() public payable {
+        require(msg.value < 0.001 ether);
+        contributions[msg.sender] += msg.value;
+        if (contributions[msg.sender] > contributions[owner]) {
+            owner = msg.sender;
+        }
+    }
+
+    function getContribution() public view returns (uint256) {
+        return contributions[msg.sender];
+    }
+
+    function withdraw() public onlyOwner {
+        payable(owner).transfer(address(this).balance);
+    }
+
+    receive() external payable {
+        require(msg.value > 0 && contributions[msg.sender] > 0);
+        owner = msg.sender;
+    }
+}
+
+// Optimism testnetwork address : 0x6B6e3687549781f14b201694c4D9522423f4DC17
